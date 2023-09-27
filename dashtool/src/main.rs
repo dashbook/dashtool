2e73299d2f339a1ce693092962d996329d1acd4a
Jan Kaul <jankaul@mailbox.org>
2023-09-27 08:54:45 +0200

    create dashtool config dir

--- dashtool/src/main.rs
+++ dashtool/src/main.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use std::{fs, str::FromStr};
 
+use anyhow::anyhow;
 use dashtool::{error::Error, init::init, run::run};
 
 use clap::{Parser, Subcommand};
@@ -21,6 +22,13 @@ async fn main() -> Result<(), Error> {
     let args = Args::parse();
 
     fs::create_dir_all(".dashtool/dags").ok();
+    #[cfg(not(target_arch = "wasm32"))]
+    fs::create_dir_all(
+        dirs::config_local_dir()
+            .and_then(|x| Some(String::from_str(x.to_str()?).ok()?))
+            .ok_or(Error::Anyhow(anyhow!("Failed to get config directory.")))?
+            + "/dashtool",
+    )?;
 
     match args.commands {
         Commands::Run => run().await,
